d5ef88e245ff5c0758ea810491785fc481eb97a1
valexsav <valexvasilevich@gmail.com>
2025-07-20 17:45:42 +0200

    Add redirect to index page. Add index page template

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
-from flask import Flask
+from flask import Flask, redirect, url_for, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/index")
 def main_page():
-    return "<h1>Hi there!</h1>"
+    return render_template("index.html")
+
+@app.route("/")
+def redirect_to_main_page():
+    return redirect(url_for("main_page"))
 
 if __name__ == "__main__":
     app.run(debug=True)
